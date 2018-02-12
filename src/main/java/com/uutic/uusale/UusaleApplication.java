078238e4dc467f4f4985acc837e9b68c149d56be
a021672 <cloudxpc@126.com>
2018-02-12 16:27:55 +0800

    Initial commit

---
+++ src/main/java/com/uutic/uusale/UusaleApplication.java
@@ -0,0 +1,12 @@
+package com.uutic.uusale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UusaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UusaleApplication.class, args);
+	}
+}
