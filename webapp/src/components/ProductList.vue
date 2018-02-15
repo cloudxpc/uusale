d63dfaaec9b60460fd218ec34cd7ee4643dc0ce4
Rick <cloudxpc@126.com>
2018-02-15 14:30:38 +0800

    search in product list page

--- webapp/src/components/ProductList.vue
+++ webapp/src/components/ProductList.vue
@@ -2,11 +2,27 @@
   <div>
     <h1 class="page_header">商品列表</h1>
     <div class="weui-panel">
+      <div class="weui-search-bar" id="searchBar">
+        <form class="weui-search-bar__form">
+          <div class="weui-search-bar__box">
+            <i class="weui-icon-search"></i>
+            <input type="search" class="weui-search-bar__input" id="searchInput" placeholder="搜索" required="" v-model="searchText">
+            <a href="javascript:" class="weui-icon-clear" id="searchClear"></a>
+          </div>
+          <label class="weui-search-bar__label" id="searchText" style="transform-origin: 0px 0px; opacity: 1; transform: scale(1, 1);">
+            <i class="weui-icon-search"></i>
+            <span>搜索</span>
+          </label>
+        </form>
+        <a href="javascript:" class="weui-search-bar__cancel-btn" id="searchCancel">取消</a>
+      </div>
       <div v-if="$eventBus.isMch" class="weui-panel__hd">
         <router-link to="/main/product/edit/new" class="weui-btn weui-btn_mini weui-btn_primary">添加商品</router-link>
       </div>
       <div class="weui-panel__bd">
-        <router-link v-for="item in products" :key="item.id" :to="'/main/product/' + ($eventBus.isMch ? 'edit' : 'view') + '/' + item.id" class="weui-media-box weui-media-box_appmsg">
+        <router-link v-for="item in filteredProducts" :key="item.id"
+                     :to="'/main/product/' + ($eventBus.isMch ? 'edit' : 'view') + '/' + item.id"
+                     class="weui-media-box weui-media-box_appmsg">
           <div class="weui-media-box__hd">
             <img class="weui-media-box__thumb" :src="getProductFirstImgSrc(item)" alt="">
           </div>
@@ -24,15 +40,25 @@
 </template>
 
 <script>
+  import weui from 'weui.js';
   export default {
     name: 'ProductList',
     data: function () {
       return {
-        products: []
+        products: [],
+        searchText: ''
       };
+    },
+    computed: {
+      filteredProducts: function () {
+        return this.searchText ? this.$_.filter(this.products, (p) => p.name.indexOf(this.searchText) >= 0) : this.products;
+      }
     },
     created: function () {
       this.init();
+    },
+    mounted: function () {
+      weui.searchBar('#searchBar');
     },
     methods: {
       init: function () {
