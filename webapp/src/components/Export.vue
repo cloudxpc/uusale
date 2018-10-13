5dd61c0867ed25d6b01250dc01e1b8249369ca50
cloudxpc <cloudxpc@126.com>
2018-10-13 19:27:17 +0800

    Create datetime control for export

--- webapp/src/components/Export.vue
+++ webapp/src/components/Export.vue
@@ -1,65 +1,115 @@
 <template>
-    <div>
-        <h1 class="page_header">历史订单导出</h1>
-        <div class="weui-cells">
-            <div class="weui-cell">
-                <div class="weui-cell__hd">
-                    <label class="weui-label">开始</label>
-                </div>
-                <div class="weui-cell__bd">
-                    <input class="weui-input" type="datetime-local" v-model="from">
-                </div>
-                <div class="weui-cell__ft"></div>
-            </div>
-            <div class="weui-cell">
-                <div class="weui-cell__hd">
-                    <label class="weui-label">结束</label>
-                </div>
-                <div class="weui-cell__bd">
-                    <input class="weui-input" type="datetime-local" v-model="to">
-                </div>
-                <div class="weui-cell__ft"></div>
-            </div>
+  <div>
+    <h1 class="page_header">历史订单导出</h1>
+    <div class="weui-cells__title">开始日期和时间</div>
+    <div class="weui-cells">
+      <a class="weui-cell weui-cell_access" @click="selectDate('from')">
+        <div class="weui-cell__bd">
+          <p>开始日期</p>
         </div>
-        <div class="weui-btn-area">
-            <button type="button" class="weui-btn weui-btn_primary" @click.prevent="submit">下载</button>
+        <div class="weui-cell__ft">{{fromDateStr ? fromDateStr : '请选择'}}</div>
+      </a>
+      <a class="weui-cell weui-cell_access" @click="selectTime('from')">
+        <div class="weui-cell__bd">
+          <p>开始时间</p>
         </div>
+        <div class="weui-cell__ft">{{fromTimeStr ? fromTimeStr : '请选择'}}</div>
+      </a>
     </div>
+    <div class="weui-cells__title">结束日期和时间</div>
+    <div class="weui-cells">
+      <a class="weui-cell weui-cell_access" @click="selectDate('to')">
+        <div class="weui-cell__bd">
+          <p>结束日期</p>
+        </div>
+        <div class="weui-cell__ft">{{toDateStr ? toDateStr : '请选择'}}</div>
+      </a>
+      <a class="weui-cell weui-cell_access" @click="selectTime('to')">
+        <div class="weui-cell__bd">
+          <p>结束时间</p>
+        </div>
+        <div class="weui-cell__ft">{{toTimeStr ? toTimeStr : '请选择'}}</div>
+      </a>
+    </div>
+    <div class="weui-btn-area">
+      <button type="button" class="weui-btn weui-btn_primary" @click.prevent="submit">下载</button>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Export',
-        data: function () {
-            return {
-                from: '',
-                to: ''
-            };
-        },
-        methods: {
-            format: function(dte) {
-                var fmt = "yyyyMMddhhmmss";
-                var o = {
-                    "M+": dte.getMonth() + 1, //月份 
-                    "d+": dte.getDate(), //日 
-                    "h+": dte.getHours(), //小时 
-                    "m+": dte.getMinutes(), //分 
-                    "s+": dte.getSeconds(), //秒 
-                    "q+": Math.floor((dte.getMonth() + 3) / 3), //季度 
-                    "S": dte.getMilliseconds() //毫秒 
-                };
-                if (/(y+)/.test(fmt))
-                    fmt = fmt.replace(RegExp.$1, (dte.getFullYear() + "").substr(4 - RegExp.$1.length));
-                for (var k in o) {
-                    if (new RegExp("(" + k + ")").test(fmt)) {
-                        fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-                    }
-                }
-                return fmt;
-            },
-            submit: function () {
-                window.location.href = this.$eventBus.baseUrl + "/report/" + this.format(new Date(this.from)) + "/" + this.format(new Date(this.to));
+  import weui from 'weui.js';
+
+  export default {
+    name: 'Export',
+    data: function () {
+      return {
+        fromDate: '',
+        fromDateStr: '',
+        fromTime: '',
+        fromTimeStr: '',
+        toDate: '',
+        toDateStr: '',
+        toTime: '',
+        toTimeStr: ''
+      };
+    },
+    methods: {
+      formatNumber: function(n) {
+        n = n.toString()
+        return n[1] ? n : '0' + n
+      },
+      selectDate: function (fromto) {
+        var today = new Date();
+        weui.datePicker({
+          start: 2018,
+          end: today,
+          defaultValue: [today.getFullYear(), today.getMonth() + 1, today.getDate()],
+          onConfirm: (result) => {
+            if (fromto === 'from') {
+              this.fromDate = result.map(a => this.formatNumber(a.value)).join('');
+              this.fromDateStr = result.map(a => this.formatNumber(a.label)).join('');
+            } else {
+              this.toDate = result.map(a => this.formatNumber(a.value)).join('');
+              this.toDateStr = result.map(a => this.formatNumber(a.label)).join('');
             }
+          },
+          id: 'datePicker' + fromto
+        });
+      },
+      selectTime: function (fromto) {
+        var hours = [];
+        var minutes = [];
+        for (var i = 0; i < 24; i++) {
+          hours.push({
+            label: i + '点',
+            value: i
+          });
         }
+        for (var j = 0; j < 60; j++) {
+          minutes.push({
+            label: j + '分',
+            value: j
+          });
+        }
+        weui.picker(hours, minutes, {
+          defaultValue: [0, 0],
+          onConfirm: (result) => {
+            if (fromto === 'from') {
+              this.fromTime = result.map(a => this.formatNumber(a.value)).join('');
+              this.fromTimeStr = result.map(a => this.formatNumber(a.label)).join('');
+            } else {
+              this.toTime = result.map(a => this.formatNumber(a.value)).join('');
+              this.toTimeStr = result.map(a => this.formatNumber(a.label)).join('');
+            }
+          },
+          id: 'timePicker' + fromto
+        });
+      },
+      submit: function () {
+        if (this.fromDate && this.toDate)
+          window.location.href = this.$eventBus.baseUrl + "/report/" + this.fromDate + this.fromTime + '00' + "/" + this.toDate + this.toTime + '00';
+      }
     }
+  }
 </script>
